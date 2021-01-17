89bc71e38fdf6070034bf247a39a8d84f2d4671c
astartafl <astartafl@gmail.com>
2021-01-17 20:19:32 +0200

    2

--- src/CalcStart.java
+++ src/CalcStart.java
@@ -5,8 +5,6 @@ public class CalcStart {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
         System.out.println(cal.calculate("a", "b", 0));
-        System.out.println(cal.calculate("a", "b", 1));
-        System.out.println(cal.calculate("a", "b", 2));
-        System.out.println(cal.calculate("a", "b", 3));
+
     }
 }
